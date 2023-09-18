48d979bfc6f91d0cd8382bf2f8b95e098ccb5458
David Mason <DIMason1702@gmail.com>
2023-09-18 14:57:46 +0100

    Ticket 3 complete

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,15 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
-    if ("9" === digit) {
+  for (let i = 0; i < number.toString().length; i++) {
+    console.log(number);
+    let digit = number.toString()[i];
+    console.log(digit);
+    if (digit === "9") {
       count++;
     }
   }
 
   return count;
 }
+console.log(countNumberOfNinesInNumber(1115.12578469))
